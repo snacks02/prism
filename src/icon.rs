992c6e863428c9e85d760d0a043d880fccecc10f
Andrei Zhigalkin <snacks64@proton.me>
2026-04-09 06:52:06 +0500

    Rename the icon folder

--- src/icon.rs
+++ src/icon.rs
@@ -1,8 +1,8 @@
-pub const FILE_PLUS: &[u8] = include_bytes!("../icons/file-plus.svg");
-pub const FOLDER_PLUS: &[u8] = include_bytes!("../icons/folder-plus.svg");
-pub const MUSIC: &[u8] = include_bytes!("../icons/music.svg");
-pub const PAUSE: &[u8] = include_bytes!("../icons/pause.svg");
-pub const PLAY: &[u8] = include_bytes!("../icons/play.svg");
-pub const SEARCH: &[u8] = include_bytes!("../icons/search.svg");
-pub const SKIP_BACK: &[u8] = include_bytes!("../icons/skip-back.svg");
-pub const SKIP_FORWARD: &[u8] = include_bytes!("../icons/skip-forward.svg");
+pub const FILE_PLUS: &[u8] = include_bytes!("../icon/file-plus.svg");
+pub const FOLDER_PLUS: &[u8] = include_bytes!("../icon/folder-plus.svg");
+pub const MUSIC: &[u8] = include_bytes!("../icon/music.svg");
+pub const PAUSE: &[u8] = include_bytes!("../icon/pause.svg");
+pub const PLAY: &[u8] = include_bytes!("../icon/play.svg");
+pub const SEARCH: &[u8] = include_bytes!("../icon/search.svg");
+pub const SKIP_BACK: &[u8] = include_bytes!("../icon/skip-back.svg");
+pub const SKIP_FORWARD: &[u8] = include_bytes!("../icon/skip-forward.svg");
